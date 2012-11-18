47df7f07fdc02ca2147719e7d9d7c4c3afb232cc
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-11-19 00:45:44 +0530

    update project template for 0.2.0 features

--- lein-template/src/leiningen/new/lein_clr/project.clj
+++ lein-template/src/leiningen/new/lein_clr/project.clj
@@ -6,7 +6,21 @@
   :dependencies []
   :min-lein-version "2.0.0"
   :plugins [[lein-clr "0.2.0-SNAPSHOT"]]
-  :clr {:main-cmd    [#_"mono" [CLJCLR14_40 "Clojure.Main.exe"]]
-        :compile-cmd [#_"mono" [CLJCLR14_40 "Clojure.Compile.exe"]]
-        ;; :target-path "target/clr"
-        :load-paths  []})
+  :clr {:cmd-templates  {:clj-exe   [#_"mono" [CLJCLR14_40 %1]]
+                         :clj-dep   [#_"mono" ["target/clr/clj/Debug 4.0" %1]]
+                         :clj-url   "https://github.com/downloads/clojure/clojure-clr/clojure-clr-1.4.0-Debug-4.0.zip"
+                         :clj-zip   "clojure-clr-1.4.0-Debug-4.0.zip"
+                         :curl      ["curl" "--insecure" "-f" "-L" "-o" %1 %2]
+                         :nuget-ver [#_"mono" [*PATH "nuget.exe"] "install" %1 "-Version" %2]
+                         :nuget-any [#_"mono" [*PATH "nuget.exe"] "install" %1]
+                         :unzip     ["unzip" "-d" %1 %2]
+                         :wget      ["wget" "--no-check-certificate" "--no-clobber" "-O" %1 %2]}
+        ;; for automatic download/unzip of ClojureCLR,
+        ;; 1. make sure you have curl or wget installed and on PATH,
+        ;; 2. uncomment deps in :deps-cmds, and
+        ;; 3. use :clj-dep instead of :clj-exe in :main-cmd and :compile-cmd
+        :deps-cmds      [; [:wget  :clj-zip :clj-url] ; edit to use :curl instead of :wget
+                         ; [:unzip "../clj" :clj-zip]
+                         ]
+        :main-cmd      [:clj-exe "Clojure.Main.exe"]
+        :compile-cmd   [:clj-exe "Clojure.Compile.exe"]})
